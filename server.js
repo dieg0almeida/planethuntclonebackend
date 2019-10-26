14eab0119b4b5c1e1cb0313e815946338945b45f
Diego Almeida <diegalmeida@hotmail.com>
2019-10-26 15:55:41 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
+
+// iniciando a aplicação
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// iniciando o bando de dados
+//var urlConnection = "mongodb://localhost:27017/node";
+
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+requireDir('./src/models');
+
+
+// primeira rota
+app.use('/api', require('./src/routes'));
+
+
+// iniciando o servidor
+app.listen(process.env.PORT || 3001);
